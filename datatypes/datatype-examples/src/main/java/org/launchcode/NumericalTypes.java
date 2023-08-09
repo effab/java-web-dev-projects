b57b2530b87937c7f5dbfecb6eb81898b4b2aa04
effab <abeffan@gmail.com>
2023-08-09 14:19:07 -0500

    mpg and numerical

---
+++ datatypes/datatype-examples/src/main/java/org/launchcode/NumericalTypes.java
@@ -0,0 +1,18 @@
+package org.launchcode;
+
+import java.util.Scanner;
+
+public class NumericalTypes {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("What is the length of the rectangle?");
+        Double length = input.nextDouble();
+
+        System.out.println("What is the width of the rectangle?");
+        Double width = input.nextDouble();
+
+        double rectangleArea = length * width;
+        System.out.println("The area of the rectangle is " + rectangleArea);
+    }
+}
---
+++ datatypes/datatype-examples/src/main/java/org/launchcode/milesPerGallon.java
@@ -0,0 +1,17 @@
+package org.launchcode;
+
+import java.util.Scanner;
+public class milesPerGallon {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("How many miles have you driven?");
+        Double numMiles = input.nextDouble();
+
+        System.out.println("How many gallons have gas have you used?");
+        Double gallons = input.nextDouble();
+
+        double milesPerGallon = numMiles / gallons;
+        System.out.println("You are running on " + milesPerGallon + " mpg.");
+        }
+}
