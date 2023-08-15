872c6eddb286233d87bf9a1436879edc73f2a341
effab <abeffan@gmail.com>
2023-08-15 15:53:41 -0500

    Array

---
+++ control-flow-and-collections/exercises/collection-exercises/src/main/java/org/example/ArrayListPractice.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import java.util.ArrayList;
+
+public class ArrayListPractice {
+    public static int
+        sumEven(ArrayList<Integer> arr) {
+            int total = o;
+            for (int integer :arr) {
+                if (integer % 2 == 0) {
+                    total += integer;
+                }
+            }
+            return total;
+        }
+    }
+
